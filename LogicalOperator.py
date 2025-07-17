dd7a84af8e9b5045a3bc9c131d8a1d5085615649
Ornab Biswass <79110271+Ornab95@users.noreply.github.com>
2025-07-18 04:12:59 +0600

    Add Logical Operator

---
+++ LogicalOperator.py
@@ -0,0 +1,10 @@
+# Logical Operator
+print(19>10) #True
+print(19<10) #False
+print(19==10) #False
+print(19!=10) #True
+print(19>10) #True
+print(19<10) #False
+print(19==10) #False
+print(19!=10) #True
+print(19>10) # True
