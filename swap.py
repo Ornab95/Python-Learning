af701b2f365d11e960d530741b68ead75ae538d8
Ornab Biswass <79110271+Ornab95@users.noreply.github.com>
2025-07-18 04:14:21 +0600

    Add Swap 2 Number

---
+++ swap.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+
+#swap
+c = a
+a = b
+b = c
+
+print("a is : ",a)
+print("b is : ",b)
+#print(c)
+
+#left,right = right,left
+a = 40
+b = 30
+a,b = b,a
+print("\n\na is : ",a)
+print("b is : ",b)
